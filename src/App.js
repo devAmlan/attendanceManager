52135bc476bbece7a5c97685d63f73fba0e90d45
Aj-github-cmd <sahooamlanjyoti99@gmail.com>
2021-09-26 23:01:51 +0530

    built json server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React,{useEffect} from 'react'
+import api from './api/students'
 function App() {
+  const getStudentsdata = async() => {
+    const resp = await api.get('/students')
+    console.log(resp.data)
+    return resp.data;
+  }
+  useEffect(()=>{
+    getStudentsdata()
+  },[])
   return (
     <div className="App">
-    
+    <h1>Hello React!!</h1>
     </div>
   );
 }
